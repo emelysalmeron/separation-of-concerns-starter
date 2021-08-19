f1e0930ffca5e56d05a7ec73dd55ae373f8e2ff6
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2021-08-19 12:31:51 +0200

    last lint error fixing

--- client/src/handlers/handler.js
+++ client/src/handlers/handler.js
@@ -3,10 +3,10 @@ const todoList = document.querySelector('.todo_list');
 
 export const addTodo = (event) => {
   event.preventDefault();
-  // todo DIV
+  // create todo DIV
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
-  // todo LI
+  // create todo LI
   const newTodo = document.createElement('li');
   newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo_item');
@@ -19,4 +19,5 @@ export const addTodo = (event) => {
   todoList.appendChild(todoDiv);
   // Clear todo input VALUE
   todoInput.value = '';
+  return todoDiv; // added this line to avoid the lint error as the function expects to have a return statement
 };
