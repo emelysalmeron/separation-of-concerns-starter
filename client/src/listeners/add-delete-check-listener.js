f1a7a1b9507b9d1728d9a1062eb66a50419a6305
seni <senaitb1@gmail.com>
2021-08-24 00:01:19 +0200

    Input max. length inculeded

--- client/src/listeners/add-delete-check-listener.js
+++
@@ -1,7 +0,0 @@
-import { addTodo, deleteCheck } from '../handlers/add-delete-check-handlers.js';
-
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo_list');
-// event listeners
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
